62c4710648425b5b74698dd875dd07a828930440
Shriram Ramamurthy <shriramr@amazon.com>
2017-05-04 09:28:03 -0700

    Modified Twilio Handler

--- src/main/java/findmyphone/TwilioHandler.java
+++ src/main/java/findmyphone/TwilioHandler.java
@@ -2,25 +2,13 @@ package findmyphone;
 
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
-import com.twilio.twiml.Say;
-import com.twilio.twiml.TwiMLException;
-import com.twilio.twiml.VoiceResponse;
+import com.amazonaws.util.json.JSONException;
+import com.amazonaws.util.json.JSONObject;
+
 
 public class TwilioHandler {
 
-    public String makeACallHandler(Context context) {
-        LambdaLogger logger = context.getLogger();
-        VoiceResponse voiceResponse = new VoiceResponse.Builder()
-            .say(new Say.Builder("Hello Monkey").build())
-            .build();
-        try {
-            return voiceResponse.toXml();
-        }
-        catch (TwiMLException e) {
-            logger.log("Twilio Exception : " + e);
-            e.printStackTrace();
-            return null;
-        }
+    public String makeACallHandler(Context context) throws JSONException {
+        return "Sherlock found your phone";
     }
 }
